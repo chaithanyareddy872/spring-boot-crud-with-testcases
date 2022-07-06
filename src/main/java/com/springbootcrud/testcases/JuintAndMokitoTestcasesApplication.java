44107fd56acade0c0b52c75b2d1229864b77d505
chaithanyareddy <chaithanyareddy872@gmail.com>
2022-07-06 18:16:31 +0530

    basic '

---
+++ src/main/java/com/springbootcrud/testcases/JuintAndMokitoTestcasesApplication.java
@@ -0,0 +1,13 @@
+package com.springbootcrud.testcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuintAndMokitoTestcasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuintAndMokitoTestcasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootcrud/testcases/JuintAndMokitoTestcasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootcrud.testcases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuintAndMokitoTestcasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
